b47e761c0cc425b6ce8e40a1f3b561de3e9b7297
Danteabad <dante.abad@institutvidreres.cat>
2021-11-23 09:22:23 +0100

    Ex2Funcions

--- EJ_2.js
+++ EJ_2.js
@@ -1,5 +1,12 @@
 //EX1
-function hola(nom, cog) {
-    console.log(nom + cog)
-}
-hola('Dante', 'Abad')
+//function hola(nom, cog) {
+//    console.log(nom + cog)
+//}
+//hola('Dante ', 'Abad')
+
+//EX2
+//function hola(nom, cog) {
+//     console.log(nom + cog)
+//}
+//hola('Dante ', 'Abad')
+//typeof hola
