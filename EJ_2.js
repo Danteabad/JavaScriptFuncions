62618ea54f68520d5a5c0f0247fbaa225181de34
Danteabad <dante.abad@institutvidreres.cat>
2021-11-29 08:58:50 +0100

    Ex9

--- EJ_2.js
+++ EJ_2.js
@@ -51,3 +51,11 @@ console.log (a+b+c)*/
     }
 }
 console.log(num(30,15))*/
+
+//EX9
+/*var num = function(a,b){
+    if (a == 30 || b == 30 || a+b == 30) {
+        return true
+    }
+}
+console.log(num(15,15))*/
